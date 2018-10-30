f0d4da98d09f7ebebc0b6e78165ab54bfe917de2
Aliaksei <alexx333by@gmail.com>
2018-10-30 13:51:58 +0300

    Tests for extended type implemented

---
+++ AssemblyBrowser/AssemblyBrowser.Model.UnitTests.ExtensionMethodsAssembly/ExtensionMethodsTestTypes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AssemblyBrowser.Model.UnitTests.ExtensionMethodsAssembly
+{
+    public interface IMyInterface
+    {
+        void MethodB();
+    }
+
+    public static class Extension
+    {
+        public static void MethodA(this IMyInterface myInterface, int i)
+        {
+            Console.WriteLine
+                ("Extension.MethodA(this IMyInterface myInterface, int i)");
+        }
+
+        public static void MethodA(this IMyInterface myInterface, string s)
+        {
+            Console.WriteLine
+                ("Extension.MethodA(this IMyInterface myInterface, string s)");
+        }
+
+        public static int ExtensionMethodsCount => 2;
+    }
+}
---
+++ AssemblyBrowser/AssemblyBrowser.Model.UnitTests/ExtensionUnitTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using AssemblyBrowser.Model.UnitTests.ExtensionMethodsAssembly;
+
+namespace AssemblyBrowser.Model.UnitTests
+{
+    [TestClass]
+    public class ExtensionUnitTest
+    {
+        AssemblyInfo assemblyInfo;
+        string fullNamespace;
+
+        [TestInitialize]
+        public void Init()
+        {
+            assemblyInfo = new AssemblyInfo(Path.GetFullPath("AssemblyBrowser.Model.UnitTests.ExtensionMethodsAssembly.dll"));
+            fullNamespace = "AssemblyBrowser.Model.UnitTests.ExtensionMethodsAssembly";
+        }
+
+        [TestMethod]
+        public void ExtensionMethodsInExtendedType()
+        {
+            AssemblyDatatypeInfo extendedDatatype = null;
+            foreach (AssemblyDatatypeInfo datatypeInfo in assemblyInfo.Namespaces[fullNamespace].Datatypes)
+            {
+                if (datatypeInfo.Name.Equals(nameof(IMyInterface)))
+                {
+                    extendedDatatype = datatypeInfo;
+                }
+            }
+            Assert.AreEqual(Extension.ExtensionMethodsCount, extendedDatatype?.ExtensionMethods.Count);
+        }
+
+        [TestMethod]
+        public void ExtensionMethodsNotInDeclaredType()
+        {
+            AssemblyDatatypeInfo declaredDatatype = null;
+            foreach (AssemblyDatatypeInfo datatypeInfo in assemblyInfo.Namespaces[fullNamespace].Datatypes)
+            {
+                if (datatypeInfo.Name.Equals(nameof(Extension)))
+                {
+                    declaredDatatype = datatypeInfo;
+                }
+            }
+            Assert.AreEqual(0, declaredDatatype?.ExtensionMethods.Count);
+        }
+    }
+}
